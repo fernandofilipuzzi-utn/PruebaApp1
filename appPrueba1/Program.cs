08e6fa046d56f5acf40a3dbad7a8da2b5da7dfbc
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-11 17:40:10 -0300

    .

---
+++ appPrueba1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace microsoft.botsay;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
